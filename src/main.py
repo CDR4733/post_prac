da37f76c1ddb548c462b492c70e0a331ccd70e56
CDR4733 <shsing2@naver.com>
2026-06-28 19:22:32 +0900

    [add] project init
    * project init

---
+++ src/main.py
@@ -0,0 +1,61 @@
+from contextlib import asynccontextmanager  # Lifespan 생성에 사용
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import (
+    CORSMiddleware,  # CORS 설정을 위한 Middleware
+)
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,  # MongoDB 연결에 사용할 motor
+)
+
+from src.apps.api import api_router  # Router 등록
+from src.core.config import MONGODB_DB, MONGODB_URL  # .env 환경변수 로드
+
+
+# APP Lifespan
+# L-1. lifespan 정의: 앱 시작과 종료 시 실행될 로직
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # A. [Stratup] 앱이 켜질 때 실행
+    app.mongodb_client = AsyncIOMotorClient(MONGODB_URL)
+    app.mongodb = app.mongodb_client.get_database(MONGODB_DB)
+    try:
+        # A-1. MongoDB 연결
+        await app.mongodb_client.admin.command("ping")
+        print("MongoDB 연결에 성공했습니다.")
+
+        # A-2. Index 설정 (Unique값 설정)
+        # 보류
+
+    except Exception as err:
+        print("MongoDB 연결에 실패했습니다, {err}")
+
+    yield  # 여기서 앱 실행
+
+    # B. [Shutdown] 앱이 꺼질 때 실행
+    app.mongodb_client.close()
+    print("MongoDB 연결이 종료되었습니다.")
+
+
+# L-2. FastAPI 인스턴스 생성 시 lifespan(L-1)을 따름
+app = FastAPI(lifespan=lifespan)
+
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 테스트용이므로 모두 허용
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Health-Check
+@app.get("/health", tags=["Health-Check"])
+async def health_check():
+    return {"status": "healthy"}
+
+
+# APP Router 등록
+app.include_router(api_router)
